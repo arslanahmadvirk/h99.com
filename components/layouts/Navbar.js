6a226653cf0dd5641cd6043ed8640f617f4174eb
arslanahmadvirk <arslanahmadv098@gmail.com>
2022-10-02 02:48:55 +0500

    update

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -202,8 +202,8 @@ function Navbar() {
               <h1 className="text-center text-xl font-semibold whitespace-nowrap text-white">
                 H99
               </h1>
-              <div className="flex space-x-4 mb-2 p-4 bg-gray-700 rounded-3xl">
-                <div className="flex flex-col items-center cursor-pointer">
+              <div className="flex space-x-4 mb-2 p-4 bg-gray-700 rounded-3xl text-white">
+                <div className="flex flex-col items-center cursor-pointer text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -211,15 +211,16 @@ function Navbar() {
                     fill="currentColor"
                     class="bi bi-shield-fill-check"
                     viewBox="0 0 16 16"
+                    className="text-white"
                   >
                     <path
                       fill-rule="evenodd"
                       d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"
                     />
                   </svg>
-                  <h1>Security</h1>
+                  <h1 className="text-white">Security</h1>
                 </div>
-                <div className="flex flex-col items-center cursor-pointer">
+                <div className="flex flex-col items-center cursor-pointer text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -227,13 +228,13 @@ function Navbar() {
                     fill="currentColor"
                     class="bi bi-bell-fill"
                     viewBox="0 0 16 16"
-                    className="text-center"
+                    className="text-center text-white"
                   >
                     <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
                   </svg>
-                  <h1>Notifications</h1>
+                  <h1 className="text-white">Notifications</h1>
                 </div>
-                <div className="flex flex-col items-center cursor-pointer">
+                <div className="flex flex-col items-center cursor-pointer text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -241,10 +242,11 @@ function Navbar() {
                     fill="currentColor"
                     class="bi bi-clock-fill"
                     viewBox="0 0 16 16"
+                    className="text-white"
                   >
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                   </svg>
-                  <h1>History</h1>
+                  <h1 className="text-white">History</h1>
                 </div>
               </div>
               <ul className="space-y-2 bg-gray-700 rounded-2xl">
